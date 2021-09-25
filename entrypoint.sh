f2b1e1dfa9f7fdb78c7173b27aa71e2f3804b3ec
James Cole <thegrumpydictator@gmail.com>
2021-09-25 05:11:47 +0200

    Update the date of the entrypoint script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Now in entrypoint.sh for Firefly III"
-echo "Entrypoint script version is 1.0.17 (2021-06-11)"
+echo "Entrypoint script version is 1.0.18 (2021-09-25)"
 echo "Running as '$(whoami)' in group '$(id -g -n)'."
 echo "Current working dir is '$(pwd)'"
 
